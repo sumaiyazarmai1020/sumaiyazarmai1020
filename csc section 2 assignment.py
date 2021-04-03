010ad0acb95abedbfb2eba6650824317cbbdaecb
sumaiyazarmai1020 <81629515+sumaiyazarmai1020@users.noreply.github.com>
2021-04-03 15:43:21 +0800

    Add files via upload

---
+++ csc section 2 assignment.py
@@ -0,0 +1,19 @@
+print("susu")
+print("computer_science")
+scare = "boo!"
+print( scare )
+a = 2
+b = 3
+c = a+b
+print(c)
+d = 12
+e = 0.5
+f = d/e
+print(f)
+g = 1
+h = 2
+i = g+h
+print(i)
+print(8%3)
+x = (2*4)*(4+2)
+print(x)
